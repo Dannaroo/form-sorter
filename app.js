7b7d9f2c7054f0ea9beb242269a0fd0fc20e9cf6
Danny Ward <liege88@gmail.com>
2018-07-18 21:52:38 +0800

    add button to sort divs by a number value within the div.

--- app.js
+++ app.js
@@ -1,58 +1,6 @@
-// const elements = document.getElementsByClassName("box");
-// const main = document.getElementsByClassName("bin")[0];
-// const children = main.children;
-// const basin = document.getElementsByClassName("basin")[0];
-// console.log(elements);
-// console.log(main);
-// console.log(children);
 
-// Sort the .box divs into alphabetical order.
-// elements = elements.sort();
-
-// Loop through .bin and append sorted .box divs to .basin
-// for (let i = 0; i < children.length; i += 1 ) {
-//
-//   basin.appendChild(elements[i].cloneNode(true));
-//   console.log(elements[i]);
-//   console.log(i);
-// }
-// ////////////////////////////////////////////
-
-//
-// var $divs = $("div.box");
-//
-// $('#alphBnt').on('click', function () {
-//     var alphabeticallyOrderedDivs = $divs.sort(function (a, b) {
-//         return $(a).find("h1").text() > $(b).find("h1").text();
-//     });
-//
-//     alphabeticallyOrderedDivs.each(function (i, item) {
-//         $(".basin").append(item);
-//         console.log("1")
-//     });
-// });
-
-// console.log("hi");
-// $("#aphaOrder .box").sort(function (a, b) {
-//     if ( ($(a).attr("data-site").toLowerCase() > $(b).attr("data-site").toLowerCase()) )  {
-//         return 1;
-//         console.log("1");
-//     } else if ( ($(a).attr("data-site").toLowerCase() == $(b).attr("data-site").toLowerCase()) ){
-//         return 0;
-//         console.log("0");
-//     } else {
-//         return -1;
-//         console.log("-1");
-//     }
-// }).each(function () {
-//     var elem = $(this);
-//     elem.remove();
-//     $(elem).appendTo("#aphaOrder");
-//     console.log("a");
-// });
-// console.log("bye");
-
- $('#alphBnt').on('click', function () {
+//Sort divs alphabetically
+$('#alphBnt').on('click', function () {
   var alphabeticallyOrderedDivs = $('.box').sort(function(a, b) {
   			return String.prototype.localeCompare.call($(a).text().toLowerCase(), $(b).text().toLowerCase());
   		});
@@ -61,4 +9,19 @@
   container.detach().empty().append(alphabeticallyOrderedDivs);
   $('body').append(container);
 
+});
+
+
+//https://stackoverflow.com/questions/43645828/sort-divs-based-on-values-of-child-elements
+//sort divs numerically
+$('#numBnt').on('click', function () {
+  $('.bin').each(function(){
+    $(this).html(
+      $(this).find('.box').sort(function(a, b) {
+        let dsa = parseInt($(a).find('h5').eq(0).text()),
+            dsb = parseInt($(b).find('h5').eq(0).text());
+        return (dsa > dsb ? 1 : (dsa < dsb) ? -1 : 0);
+      })
+    );
+  });
 });
